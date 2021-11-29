9fde66b8a9652d9927b0588dff8018f2b3d789ce
sivakumarrdckap <sivakumarr@dckap.com>
2021-11-29 11:37:42 +0530

    7lines

--- Homepage/Components/BestVape.jsx
+++ Homepage/Components/BestVape.jsx
@@ -9,13 +9,7 @@ export default function BestVape() {
                         <h2>
                             <span>Best Vape of 2020</span>
                         </h2>
-                        <a
-                            className="button-link slide-btn hidden-xs btn-lignt"
-                            href="#best-vape"
-                            alt="Shop now"
-                        >
-                            Shop now
-                        </a>
+                        
                     </div>
                 </div>
                 <div className="col-sm-6">
